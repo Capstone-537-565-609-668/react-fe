5c8982bc1951f320c84f349f8f95f68b51f4da9a
Ankur-Datta-4 <ankurr2002@gmail.com>
2023-09-03 21:56:10 +0530

    [add] props

--- src/components/generator-dashboard/MapOutput.js
+++ src/components/generator-dashboard/MapOutput.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-const MapOutput = () => {
+const MapOutput = ({ data }) => {
+  // data is an array of polygons like:
+  //   const wktData =
+  // "POLYGON ((23.912143528942327 32.23638576333265, 62.9449272526509 65.94496225071073, 39.57411814697713 45.06330482854103, 23.912143528942327 32.23638576333265))";
   return <div className="col-span-1">MapOutput</div>;
 };
 
--- src/components/generator-dashboard/index.js
+++ src/components/generator-dashboard/index.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import GeneratorForm from "./GeneratorForm";
 import MapOutput from "./MapOutput";
 
 export default function GeneratorDashboard() {
+  const [data, setData] = useState(null);
+
   return (
     <div className="w-full min-h-screen">
       <div className="grid grid-cols-2">
         <GeneratorForm />
-        <MapOutput />
+        <MapOutput data={data} />
       </div>
     </div>
   );
