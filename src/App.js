d59fbdaf943ce523049921c3857075fc9fa10cd5
Veeresh R G <veeresh.ravi@gmail.com>
2023-11-09 22:28:48 +0530

    [ADD] Realistic Polygon UI

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import GeneratorDashboard from "./components/generator-dashboard";
 import { useState } from "react";
 import PointVor from "./components/generator-point-vor";
 import PointHull from "./components/generator-point-hull";
+import RealisticPoly from "./components/generator-realistic-poly";
 function App() {
   const [algorithm, setAlgorithm] = useState("poly");
-  const handleChange = (e) => {
+  const [area, setArea] = useState("parks");
+  const handleChange_gen = (e) => {
     setAlgorithm(e.target.value);
     console.log(e.target.value);
   };
+
   return (
     <div className="flex">
       <div>
         <div className="bg-slate-100 min-h-screen">
           {/* <h1 className="text-xl"></h1> */}
 
-          <select onChange={handleChange} className="mt-10">
+          <select onChange={handleChange_gen} className="mt-10">
             <option value={"poly"}>Polygon Generation</option>
             <option value={"point_vor"}>Point Generation - Voronoi</option>
             <option value={"point_hull"}>Point Generation - Convex Hull</option>
+            <option value={"realistic_poly"}>
+              Realistic Polygon Generation
+            </option>
           </select>
         </div>
       </div>
@@ -26,6 +32,7 @@ function App() {
         {algorithm === "poly" && <GeneratorDashboard />}
         {algorithm === "point_vor" && <PointVor />}
         {algorithm === "point_hull" && <PointHull />}
+        {algorithm === "realistic_poly" && <RealisticPoly />}
       </div>
     </div>
   );
